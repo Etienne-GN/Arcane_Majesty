5883db234851ff7e1efbea65cd20f2daca9c8150
Etienne Gagne Nadeau <etienne.gagnenadeau@gmail.com>
2026-08-17 11:53:36 -0400

    feat(amo): immersive fullscreen shell + launcher icon

--- apps/amo/android/app/src/main/java/com/arcanemajesty/amo/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.arcanemajesty.amo;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
---
+++ apps/amo/android/app/src/main/java/com/arcanemajesty/amo/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.arcanemajesty.amo
+
+import android.os.Bundle
+import android.view.View
+import com.getcapacitor.BridgeActivity
+
+class MainActivity : BridgeActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        hideSystemBars()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemBars()
+    }
+
+    private fun hideSystemBars() {
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        )
+    }
+}
